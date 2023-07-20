ec249527807711186bed8c249d0acae038060c70
Thalys001 <thalyssouza708@gmail.com>
2023-07-20 00:15:39 -0300

    feat: added sounds in the application

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 import Controls from "./controls.js";
 import Timer from "./timer.js";
+import Sound from "./sounds.js";
+import Events from "./events.js";
 import {
   buttonPlay,
   buttonPause,
   buttonStop,
   buttonSet,
-  buttonSoundOn,
-  buttonSoundOff,
   minutesDisplay,
   secondsDisplay
 } from './elements.js';
@@ -24,39 +24,6 @@ const timer = Timer({
   resetControls: controls.reset,
 });
 
-buttonPlay.addEventListener("click", function () {
-  controls.play();
-  timer.countdown();
-});
+const sound = Sound()
 
-buttonPause.addEventListener("click", function () {
-  controls.pause()
-  timer.hold();
-});
-
-buttonStop.addEventListener("click", function () {
-  controls.reset();
-  timer.reset();
-});
-
-buttonSoundOff.addEventListener("click", function () {
-  buttonSoundOn.classList.remove("hide");
-  buttonSoundOff.classList.add("hide");
-});
-
-buttonSoundOn.addEventListener("click", function () {
-  buttonSoundOn.classList.add("hide");
-  buttonSoundOff.classList.remove("hide");
-});
-
-buttonSet.addEventListener("click", function () {
-  let newMinutes = controls.getMinutes();
-
-  if (!newMinutes) {
-    timer.reset();
-    return;
-  }
-
-  timer.updateDisplay(newMinutes, 0);
-  timer.updateMinutes(newMinutes);
-});
+Events({controls, timer, sound})
